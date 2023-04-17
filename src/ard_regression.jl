e009c04174de4adc03231fe6c2f221edcd8754c0
Yoel Sanchez Araujo <yoelsanchez006@gmail.com>
2023-04-17 13:25:42 -0400

    Update ard_regression.jl

--- src/ard_regression.jl
+++ src/ard_regression.jl
@@ -1,95 +1,9 @@
-using LinearAlgebra, Convex, SCS
-
-
-"""
-y: vector of target values
-X: design matrix
-sig2y: assumed noise in y
-gammas: diagonal of regularization matrix
-n_samp: number of samples
-"""
-function weighted_x(y, X, sig2y, gammas, n_samp)
-    return 1 / sig2y * I(n_samp) .+ X * (gammas .* X')
-end
-
-"""
-X: design matrix
-y: vector of target values
-"""
-function fit_ard(X, y;  max_iter=100, tol = 1e-2, verbose = false)
-    n_samp, n_col = size(X)
-
-    z = ones(n_col)
-
-    w = Variable(n_col)
-
-    sig2y = 1.
-
-    problem = minimize(
-        sum(square(y - X*w)) + 2 * 1 / sig2y * sum(sqrt.(z) .* abs.(w))
-    )
-
-    solve!(problem, SCS.Optimizer; silent_solver = !verbose)
-
-    gammas = 1 ./ sqrt.(z) .* abs.(evaluate(w))
-
-    S = weighted_x(y, X, sig2y, gammas, n_samp)
-
-    S_inv = pinv(S)
-
-    C = diagm(gammas) .- diagm(gammas) * X' * S_inv * X * diagm(gammas)
-
-    z = diag(X' * S_inv * X)
-
-    gammas_prev = rand(n_col) * 10000
-
-    for iter in 1:max_iter
-        sig2y = norm(y .- X*evaluate(w), 2)^2 / (n_samp - sum(1 .- gammas .* diag(C)))
-
-        problem = minimize(
-            sum(square(y - X*w)) + 2 * 1 / sig2y * sum(sqrt.(z) .* abs.(w))
-        )
-
-        solve!(problem, SCS.Optimizer; silent_solver = !verbose)
-
-        gammas = 1 ./ sqrt.(z) .* abs.(evaluate(w))
-
-        S = weighted_x(y, X, sig2y, gammas, n_samp)
-
-        S_inv = pinv(S)
-
-        C = diagm(gammas) .- diagm(gammas) * X' * S_inv * X * diagm(gammas)
-
-        z = diag(X' * S_inv * X)
-
-        if norm(gammas_prev .- gammas) < tol 
-            println(string("converged in $iter", " iterations"))
-            break 
-        end
-
-        gammas_prev = gammas
-    end
-
-    opt_S_inv = pinv(weighted_x(y, X, sig2y, gammas, n_samp))
-
-    w_ard = diagm(gammas) * X' * opt_S_inv * y
-
-    C = diagm(gammas) .- diagm(gammas) * X' * opt_S_inv * X * diagm(gammas)
-
-    sig2y = norm(y .- X*w_ard, 2)^2 / (n_samp - sum(1 .- gammas .* diag(C)))
-
-    return (
-        w = w_ard, 
-        gamma = gammas, 
-        C = C,
-        sig2y = sig2y
-    )
-
-end
+using LinearAlgebra, Distributions
+using StatsBase;
 
 sse(x, y) = sum((x .- y).^2)
 
-function fit_ard_mackay(X, y; tol=1e-2,  max_iter=50, max_alpha=100)
+function fit_ard(X, y; tol=1e-2,  max_iter=50, max_alpha=100)
     n_samp, n_col = size(X)
 
     alphas = ones(n_col)
@@ -134,7 +48,7 @@ function fit_ard_mackay(X, y; tol=1e-2,  max_iter=50, max_alpha=100)
 
         mu[.!keep_coefs] .= 0
 
-        if abs(ml[iter] - ml[iter-1]) < tol
+        if abs(lml[iter] - lml[iter-1]) < tol
             println(string("converged in $iter", " iterations"))
             break
         end
