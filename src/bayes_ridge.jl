63e21fef9e239f463b11790968c19fb078742400
Yoel Sanchez Araujo <yoelsanchez006@gmail.com>
2023-04-28 10:53:26 -0400

    Create bayes_ridge.jl

---
+++ src/bayes_ridge.jl
@@ -0,0 +1,66 @@
+using LinearAlgebra, Distributions
+using StatsBase;
+
+sse(x, y) = sum((x .- y).^2)
+
+function fit_bayes_ridge(X, y; tol=1e-2,  max_iter=100)
+    n_samp, n_col = size(X)
+
+    mu = zeros(n_col)
+
+    lml = fill(NaN, max_iter)
+
+    I_n = I(n_col)
+
+    w = (X'X + I_n) \ X'y
+
+    sig2y = sse(y, X*w)
+
+    alpha = sig2y
+
+    S = pinv(1 / sig2y * X'X .+ I_n * alpha)
+
+    mu = 1 / sig2y * S * X' * y
+
+    gammas = 1 .- alpha .* diag(S)
+
+    alpha = (n_col - alpha * tr(S)) ./ sum(mu.^2)
+
+    sig2y = sse(y, X*mu) / (n_samp - sum(gammas))
+
+    params = [alpha, sig2y]
+
+    lml[1] = -log(det(S)) - n_samp * log(1/sig2y) - n_samp * log(alpha)
+
+    for iter in 2:max_iter
+
+        S = pinv(1 / sig2y * X'X .+ I_n * alpha)
+
+        mu = 1 / sig2y * S * X' * y
+    
+        gammas = 1 .- alpha .* diag(S)
+
+        alpha = (n_col - alpha * tr(S)) ./ sum(mu.^2)
+
+        sig2y = sse(y, X*mu)  / (n_samp - sum(gammas))
+
+        lml[iter] = -log(det(S)) - n_samp * log(1/sig2y) - n_samp * log(alpha)
+
+        if norm(params .- [alpha, sig2y]) < tol
+            println(string("converged in $iter", " iterations"))
+            break
+        end
+
+        params = [alpha, sig2y]
+
+    end
+
+    return (
+        sig2y = sig2y,
+        w = mu, 
+        alpha = alpha,
+        covar = S, 
+        lml = lml[.!isnan.(lml)]
+    )
+
+end
