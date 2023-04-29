bb94b202cb19f5b62ac3a8a2f4bfeabc420fb764
Yoel Sanchez Araujo <yoelsanchez006@gmail.com>
2023-04-29 19:51:04 -0400

    translation of bayes ridge into python - numpy

---
+++ src/bayes_ridge.py
@@ -0,0 +1,62 @@
+import numpy as np
+
+def sse(x, y):
+    return np.sum((x - y)**2)
+
+def fit_bayes_ridge(X, y, tol=1e-2, max_iter=100, pen_init=10):
+    n_samp, n_col = np.shape(X)
+
+    mu = np.zeros(n_col)
+
+    I_n = np.eye(n_col)
+
+    w = np.linalg.lstsq(X.T @ X + I_n * pen_init, X.T @ y, rcond=None)[0]
+
+    sig2y = see(y, X @ w)
+
+    alpha = pen_init / sig2y
+
+    covar = np.linalg.pinv( 1 / sig2y * X.T @ X + I_n * alpha)
+
+    mu = 1 / sig2y * covar @ X.T @ y
+
+    gammas = 1 - alpha * np.diag(covar)
+
+    alpha = (n_col - alpha * np.trace(covar)) / np.sum(mu**2)
+
+    sig2y = sse(y, X @ mu) / (n_samp - np.sum(gammas))
+
+    lml = np.full(max_iter, np.nan)
+
+    lml[0] = -np.log(np.linalg.det(covar)) - n_samp * np.log(1/sig2y) - n_samp *  np.log(alpha)
+
+    params = np.array([alpha, sig2y])
+
+    for iter_j in range(1, max_iter):
+        covar = np.linalg.pinv( 1 / sig2y * X.T @ X + I_n * alpha)
+
+        mu = 1 / sig2y * covar @ X.T @ y
+
+        gammas = 1 - alpha * np.diag(covar)
+
+        alpha = (n_col - alpha * np.trace(covar)) / np.sum(mu**2)
+
+        sig2y = sse(y, X @ mu) / (n_samp - np.sum(gammas))
+ 
+        lml[iter_j] = -np.log(np.linalg.det(covar)) - n_samp * np.log(1/sig2y) - n_samp *  np.log(alpha)
+
+        if np.linalg.norm( params - np.array([alpha, sig2y])) < tol:
+            print(f"converged in {iter_j} iterations")
+            break
+
+        params = np.array([alpha, sig2y])
+
+    estimates =  {
+        "sig2y" : sig2y, 
+        "w" : mu,
+        "covar" : covar, 
+        "alpha" : alpha,
+        "lml" : lml[~np.isnan(lml)]
+    }
+
+    return estimates
